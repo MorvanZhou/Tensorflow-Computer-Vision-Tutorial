e708bcc5f28e2d3305e46144fbdc305e96155202
morvanzhou <morvanzhou@gmail.com>
2018-06-28 14:14:54 +0800

    add deep dream

--- codes/show_result.py
+++ codes/show_result.py
@@ -2,6 +2,24 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 
+def deep_dream():
+    contents = ['morvan1', 'sky']
+    channels = [60, 61, 62, 121, 122, 123]
+    plt.figure(1, figsize=(4, 12))
+    plt.suptitle('mixed4d_3x3_bottleneck_pre_relu')
+    for i in range(6):
+        for j in range(2):
+            plt.subplot(6, 2, 2*i+j+1)
+            path = '../results/%s_mixed4d_3x3_bottleneck_pre_relu_%i.jpeg' % (contents[j], channels[i])
+            image = Image.open(path)
+            plt.imshow(image)
+            if j == 0:
+                plt.ylabel('channel %i' % channels[i])
+            plt.xticks(())
+            plt.yticks(())
+    plt.savefig('../results/sum_deepdream.png', dpi=500)
+
+
 def style_transfer():
     # plotting
     content = Image.open('../example_images/morvan3.jpg').resize((400, 400))
@@ -30,4 +48,5 @@ def style_transfer():
 
 
 if __name__ == '__main__':
-    style_transfer()
+    deep_dream()
+    # style_transfer()
